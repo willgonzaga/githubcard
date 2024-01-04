7305e8a20f9b9f5460bf6951fb3d94dca810c6eb
vailei <willey.oliveira12@gmail.com>
2024-01-03 22:14:31 -0300

    Changes home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                 let user = document.getElementById('user').value;
                 let repo = document.getElementById('repo').value;
 
-                document.getElementById('formGithubCard').action = `api/githubcard?${user}/${repo}`;
+                document.getElementById('formGithubCard').action = `api/githubcard?user=${user}/repo=${repo}`;
                 document.getElementById('formGithubCard').submit();
             }
         </script>
